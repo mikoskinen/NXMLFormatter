9c924935f5b246eebf782baac6673dbd2728dc33
Mikael Koskinen <mijapkos@gmail.com>
2015-11-26 13:47:49 +0200

    First version of NXMLFormatter

---
+++ NXMLFormatter/Formatter.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace NXMLFormatter
+{
+    public class Formatter
+    {
+        private static readonly XmlWriterSettings settings = new XmlWriterSettings()
+        {
+            OmitXmlDeclaration = true,
+            Indent = true,
+            NewLineOnAttributes = true
+        };
+
+        public static string Format(string source, XmlWriterSettings customSettings = null)
+        {
+            var stringBuilder = new StringBuilder();
+
+            var element = XElement.Parse(source);
+
+            using (var xmlWriter = XmlWriter.Create(stringBuilder, customSettings ?? settings))
+            {
+                element.Save(xmlWriter);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
